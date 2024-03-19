8252e634d43b1c9938825c0648c5c6747286ac37
LUCAS ARRUDA DA CRUZ <lucas.acruz@inss.gov.br>
2024-03-19 09:50:18 -0300

    update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 def separa_valores(linha):
@@ -19,20 +20,64 @@ def copia_dados(planilha1, planilha2, linha1, linha2):
     return planilha1
 
 
+def mac_eh_valido(mac):
+    return bool(re.match(r'^([0-9A-Fa-f]{2}[ :\-]?){5}([0-9A-Fa-f]{2})$', mac))
+
+
+def verifica_mac(planilha):
+    for coluna in planilha.columns:
+        contem_macs = planilha[coluna].apply(lambda x: isinstance(x, str) and mac_eh_valido(x)).any()
+        if contem_macs:
+            print(coluna)
+            return coluna
+    else:
+        return "Nenhuma coluna encontrada com valores de MAC"
+    
+
+def armazena_planilha(pergunta="Arraste aqui o arquivo desejado: "):
+    caminho_principal = str(input(pergunta))
+    caminho_principal_formatado = r"{}".format(re.sub(r"^\s*&\s*'|'\s*$", "", caminho_principal))
+    xls = pd.ExcelFile(caminho_principal_formatado)
+    nome_sheets = xls.sheet_names
+    sheet_name = mostra_abas(nome_sheets)
+    return pd.read_excel(caminho_principal_formatado, sheet_name=sheet_name, dtype=str)
+
+
+def separa_colunas(planilha, mensagem="Digite o numero das colunas que serão armazenadas os valores: "):
+    colunas = planilha.columns
+    mostra_abas(colunas)
+    colunas = input(mensagem)
+
+
+def mostra_abas(planilha):
+    abas = []
+    for contador, aba in enumerate(planilha, start=1):
+        abas.append(aba)
+        print(f"{contador} - {aba}")
+    index_value = str(input("Digite o número da aba que vai ser adicionado os valores: "))
+    if index_value.isdigit():
+        return str(abas[int(index_value) - 1])
+    else:
+        numeros = re.findall(r'\d+', index_value)
+        
+
+
 def percorre_macs():
-    sheet_name = str(input("Digite o nome da aba que vai ser adicionado os valores: "))
-    principal = pd.read_excel("C:/Users/LUCASARRUDADACRUZ/Downloads/Trabalho Cristiano/Inventário de Ativos.xlsx", sheet_name=sheet_name, dtype=str)
-    informacoes = pd.read_excel("C:/Users/LUCASARRUDADACRUZ/Downloads/Trabalho Cristiano/STAR.xlsx", dtype=str)
+    principal = armazena_planilha()
+    informacoes = armazena_planilha()
+
+    coluna_mac_principal = verifica_mac(principal)
+    coluna_mac_info = verifica_mac(informacoes)
 
     principal["SO do STAR"] = ""
     principal["STAR"] = ""
     principal["Patrimônio no STAR"] = ""
 
-    principal["Levantamento de Ativos - MACs"] = principal["Levantamento de Ativos - MACs"].astype(str)
-    informacoes["mac"] = informacoes["mac"].astype(str)
+    principal[coluna_mac_principal] = principal[coluna_mac_principal].astype(str)
+    informacoes[coluna_mac_info] = informacoes[coluna_mac_info].astype(str)
 
-    for linha_principal, valor_principal in enumerate(principal["Levantamento de Ativos - MACs"]):
-        for linha_info, valor_info in enumerate(informacoes["mac"]):
+    for linha_principal, valor_principal in enumerate(principal[coluna_mac_principal]):
+        for linha_info, valor_info in enumerate(informacoes[coluna_mac_info]):
             if valor_principal == "nan":
                 continue
             elif (",") in valor_info:
@@ -51,7 +96,7 @@ def percorre_macs():
 
 def salva_planilha(planilha):
     try:
-        planilha.to_excel("C:/Users/LUCASARRUDADACRUZ/Downloads/Trabalho Cristiano/Resultado_Rio.xlsx", index=False)
+        planilha.to_excel("Resultado.xlsx", index=False)
         print("Planilha salva com sucesso!")
     except Exception as e:
         print(e)
